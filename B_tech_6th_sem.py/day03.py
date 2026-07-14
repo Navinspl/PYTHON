ddc9dc9983cfeafb08ed0d1f9f9790c268c72b8e
NAVIN KUMAR <navinspl012@gmail.com>
2026-07-14 10:28:46 +0530

    Added python code

--- B_tech_6th_sem.py/day03.py
+++ B_tech_6th_sem.py/day03.py
@@ -1,36 +1,36 @@
-# # a,b,c=10,20,30
-# # if a>b:
-# #     if a>c:
-# #         print("a is largest Number")
-# # else:
-# #     if b>c:
-# #         print("b is largest number")
-# #     else:
-# #         print("C is largest number")
+a,b,c=10,20,30
+if a>b:
+    if a>c:
+        print("a is largest Number")
+else:
+    if b>c:
+        print("b is largest number")
+    else:
+        print("C is largest number")
 
 
-# # marks = int(input("Enter your marks:"))
-# # if (0<= marks and marks>40):
-# #     print("The grade is D")
-# # elif (40<= marks and marks >60):
-# #     print("The grade is C")
-# # elif (60<= marks  and marks>80):
-# #     print("The grade is B")
-# # elif (80 <= marks and marks >=100):
-# #     print("The grade is A")
+marks = int(input("Enter your marks:"))
+if (0<= marks and marks>40):
+    print("The grade is D")
+elif (40<= marks and marks >60):
+    print("The grade is C")
+elif (60<= marks  and marks>80):
+    print("The grade is B")
+elif (80 <= marks and marks >=100):
+    print("The grade is A")
        
-# # function
-# # block of code is known AS function
-# # Type 1
-# def Bpmce():
-#     print("Bpmce python")
-# Bpmce()    
+function
+# block of code is known AS function
+# Type 1
+def Bpmce():
+    print("Bpmce python")
+Bpmce()    
 
-# # there are four types 
-# # Q . Type 2
-# def s(a):
-#     return  a
-# s(23)
+# there are four types 
+# Q . Type 2
+def s(a):
+    return  a
+s(23)
 
 def self():
     print("Navin 23157128011 cse(ai&ml)")
