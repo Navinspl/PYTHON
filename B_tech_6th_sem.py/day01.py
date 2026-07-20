7d4a85cd8335e390198af69b35359cf9f6a1ebb4
NAVIN KUMAR <navinspl012@gmail.com>
2026-07-20 12:05:48 +0530

    Added solution

--- B_tech_6th_sem.py/day01.py
+++ B_tech_6th_sem.py/day01.py
@@ -37,39 +37,4 @@ print(a/b)
 print(a<12 and b>12)
 print(a<10 or b>34)
 
-
-# assignment Operator
-
-a+=10
-
-print(a)
-
-name="Abhishek"
-print(name)
-
-name=input("Enter your name:")
-roll=int(input("Enter your roll number:"))
-branch = input("Enter your branch:")
-age = input("Enter your age:")
-
-print(name,roll,branch,age)
-print(type(name))
-print(type(roll))
-print(type(branch))
-print(type(age))
-
-age = int(input("Enter your age:"))
-if age >=18:
-    print("Eligible for voting")
-else:
-    print("Not Eligible for voting")
-
-
-Num= int(input("Enter any number:"))
-if Num %3 ==0 and Num %5==0:
-    print("Number is divisible by 3 and 5 ")
-else:
-        print(" Number is not divisible by 3 and 5 ")
-
-    # h w  if else condition use question frame 
-    #  
+ 
