b67add8d83482953533d05a1a487e62395ff929b
NAVIN KUMAR <navinspl012@gmail.com>
2026-07-21 15:26:37 +0530

    Added solution

--- B_tech_6th_sem.py/day05.py
+++ B_tech_6th_sem.py/day05.py
@@ -8,69 +8,71 @@ for i in range(1,n+1):
 for i in range(n,0,-1):
     print(" "*(n-i),"*"*(2*i-1))
 
-# # factorial
-# fact=1
-# for i in range(1,n+1):
-#     fact *=i
+# factorial
+fact=1
+for i in range(1,n+1):
+    fact *=i
 
-# print(fact)
+print(fact)
 
-# # f string use c
-# print(f'{"name "}')
+# f string use c
+print(f'{"name "}')
 
 # prime number
 
-# if n<=1:
-#     print("Not a prime number.")
-# else:
-#     for i in range(2,int(n**0.5)+1):
-#         if n%i ==0:
-# #             print("Not a prime number.")
-# #     else:
-# #         print("Prime Number.")    
+if n<=1:
+    print("Not a prime number.")
+else:
+    for i in range(2,int(n**0.5)+1):
+        if n%i ==0:
+            print("Not a prime number.")
+    else:
+        print("Prime Number.")    
 
 
-# count = 0
+count = 0 
 
-# for i in range(1, n + 1):
-#     if n % i == 0:
-#         count = count + 1
-# if count == 2:
-#     print("Prime Number.") 
+for i in range(1, n + 1):
+    if n % i == 0:
+        count = count + 1
+if count == 2:
+    print("Prime Number.") 
 
-# else:
-#     print("Not a prime Number.") 
+else:
+    print("Not a prime Number.") 
 
-# # fibbonaci series 
-# a,b=0,1
+# fibbonaci series 
+a,b=0,1
 
-# print("Fibonnaci series")
-# for i in range(n):
-#     print(a,end=" ")
-#     c = a+b
-#     a=b
-#     b=c 
+print("Fibonnaci series")
+for i in range(n):
+    print(a,end=" ")
+    c = a+b
+    a=b
+    b=c 
 
-# for i in range(n):
-#     print(i)
-#     if i==5:
-#         break
+for i in range(n):
+    print(i)
+    if i==5:
+        break
 # print 1 to 50 skip 20
-# for i in range(n):
-#     if i==20:
-#         continue
-#     if i%2 ==0:
-#         print(i)
+for i in range(n):
+    if i==20:
+        continue
+    if i%2 ==0:
+        print(i)
    
 
-# for i in range(1,11):
-#     n=2
-#     print(f"{n}*{i} = {n*i}")
+for i in range(1,11):
+    n=2
+    print(f"{n}*{i} = {n*i}")
  
 for i in range(1,11):
     n1,n2,n=3,4,2
     print(f"{n}*{i} = {n*i} ,{n1}*{i} = {n1*i},{n2}*{i} = {n2*i}")
  
-# for i in range(1,11):
-#     n2=4
-#     print(f"{n}*{i} = {n*i}")
+for i in range(1,11):
+    n2=4
+    print(f"{n}*{i} = {n*i}")
+
+
--- B_tech_6th_sem.py/day06.py
+++ B_tech_6th_sem.py/day06.py
@@ -1,24 +1,24 @@
-# # oddEven and prime using function
+# oddEven and prime using function
 
-# n = int(input("Enter anuy Number:"))
-# def EvenOdd(n):
+n = int(input("Enter anuy Number:"))
+def EvenOdd(n):
     
-#     if n%2==0:
-#         print("The given number is Even")
-#     else:
-#         print("The given number is Odd")
-# def Prime(n):
-#     count=0
-#     for i in range(1,n+1):
-#         if n%i==0:
-#             count = count+1
-#     if count == 2:
-#         print("The Given number is prime.")
-#     else:
-#         print("The given number is not prime.")
+    if n%2==0:
+        print("The given number is Even")
+    else:
+        print("The given number is Odd")
+def Prime(n):
+    count=0
+    for i in range(1,n+1):
+        if n%i==0:
+            count = count+1
+    if count == 2:
+        print("The Given number is prime.")
+    else:
+        print("The given number is not prime.")
 
-# EvenOdd(n)
-# Prime(n)
+EvenOdd(n)
+Prime(n)
 
 n = "BPMCE MADHEPURA "
 B = " AI & ML"
